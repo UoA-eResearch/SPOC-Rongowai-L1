cdf00e66decba961b09a899431e8a2fd05b097c6
mike-ivs <mike.laverick@auckland.ac.nz>
2023-06-29 17:07:00 +1200

    enforce NaN on netcdf fill

--- sxs/load_files.py
+++ sxs/load_files.py
@@ -449,7 +449,7 @@ def write_netcdf(dict_in, definition_file, output_file):
                     var_k[()] = v
             else:  # variable
                 var_k = ncfile.createVariable(
-                    k, get_datatype(ds_k), get_dimensions(ds_k)
+                    k, get_datatype(ds_k), get_dimensions(ds_k), fill_value=np.nan
                 )
                 var_k.units = ds_k["Units"].values[0]
                 var_k.long_name = ds_k["Long_name"].values[0]
