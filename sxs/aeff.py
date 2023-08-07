244dca2d744d5f413576284f3799eb0e23d99e3c
Xander Cai <xandcai@gmail.com>
2023-08-07 13:27:53 +1200

    add get_ddma_v1 and get_ddma_v2 functions

--- sxs/aeff.py
+++ sxs/aeff.py
@@ -133,6 +133,267 @@ def get_ddm_Aeff5(
     return A_eff
 
 
+def get_ddma_v1(brcs_copol, brcs_xpol, A_eff, sp_delay_row, sp_doppler_col):
+    """
+    this function gets the brcs and A_eff within ddma region - SP bin
+
+    Parameters
+    ----------
+    brcs_copol
+    brcs_xpol
+    A_eff
+    sp_delay_row
+    sp_doppler_col
+
+    Returns
+    -------
+    brcs_copol_ddma
+    brcs_xpol_ddma
+    A_eff_ddma
+    """
+    delay_intg = math.floor(sp_delay_row)
+    delay_frac = sp_delay_row - math.floor(sp_delay_row)
+
+    doppler_intg = round(sp_doppler_col)
+    doppler_frac = sp_doppler_col - round(sp_doppler_col)
+
+    # NBRCS SP bin
+    if doppler_frac >= 0:
+
+        if doppler_intg <= 3:
+            brcs_copol_ddma = (
+                    (1 - doppler_frac) * (1 - delay_frac) * brcs_copol[doppler_intg, delay_intg] +
+                    (1 - doppler_frac) * delay_frac * brcs_copol[doppler_intg, delay_intg + 1] +
+                    doppler_frac * (1 - delay_frac) * brcs_copol[doppler_intg + 1, delay_intg] +
+                    doppler_frac * delay_frac * brcs_copol[doppler_intg + 1, delay_intg + 1]
+            )
+            brcs_xpol_ddma = (
+                    (1 - doppler_frac) * (1 - delay_frac) * brcs_xpol[doppler_intg, delay_intg] +
+                    (1 - doppler_frac) * delay_frac * brcs_xpol[doppler_intg, delay_intg + 1] +
+                    doppler_frac * (1 - delay_frac) * brcs_xpol[doppler_intg + 1, delay_intg] +
+                    doppler_frac * delay_frac * brcs_xpol[doppler_intg + 1, delay_intg + 1]
+            )
+            A_eff_ddma = (
+                    (1 - doppler_frac) * (1 - delay_frac) * A_eff[doppler_intg, delay_intg] +
+                    (1 - doppler_frac) * delay_frac * A_eff[doppler_intg, delay_intg + 1] +
+                    doppler_frac * (1 - delay_frac) * A_eff[doppler_intg + 1, delay_intg] +
+                    doppler_frac * delay_frac * A_eff[doppler_intg + 1, delay_intg + 1]
+            )
+
+        elif doppler_intg > 3:
+
+            brcs_copol_ddma = ((1 - delay_frac) * brcs_copol[doppler_intg, delay_intg] +
+                               delay_frac * brcs_copol[doppler_intg, delay_intg + 1])
+
+            brcs_xpol_ddma = ((1 - delay_frac) * brcs_xpol[doppler_intg, delay_intg] +
+                              delay_frac * brcs_xpol[doppler_intg, delay_intg + 1])
+
+            A_eff_ddma = ((1 - delay_frac) * A_eff[doppler_intg, delay_intg] +
+                          delay_frac * A_eff[doppler_intg, delay_intg + 1])
+
+    elif doppler_frac < 0:
+
+        if doppler_intg >= 1:
+
+            brcs_copol_ddma = ((1 - abs(doppler_frac)) * (1 - delay_frac) * brcs_copol[doppler_intg, delay_intg] +
+                               (1 - abs(doppler_frac)) * delay_frac * brcs_copol[doppler_intg, delay_intg + 1] +
+                               abs(doppler_frac) * (1 - delay_frac) * brcs_copol[doppler_intg - 1, delay_intg] +
+                               abs(doppler_frac) * delay_frac * brcs_copol[doppler_intg - 1, delay_intg + 1])
+
+            brcs_xpol_ddma = ((1 - abs(doppler_frac)) * (1 - delay_frac) * brcs_xpol[doppler_intg, delay_intg] +
+                              (1 - abs(doppler_frac)) * delay_frac * brcs_xpol[doppler_intg, delay_intg + 1] +
+                              abs(doppler_frac) * (1 - delay_frac) * brcs_xpol[doppler_intg - 1, delay_intg] +
+                              abs(doppler_frac) * delay_frac * brcs_xpol[doppler_intg - 1, delay_intg + 1])
+
+            A_eff_ddma = ((1 - abs(doppler_frac)) * (1 - delay_frac) * A_eff[doppler_intg, delay_intg] +
+                          (1 - abs(doppler_frac)) * delay_frac * A_eff[doppler_intg, delay_intg + 1] +
+                          abs(doppler_frac) * (1 - delay_frac) * A_eff[doppler_intg - 1, delay_intg] +
+                          abs(doppler_frac) * delay_frac * A_eff[doppler_intg - 1, delay_intg + 1])
+
+        elif doppler_intg < 1:
+
+            brcs_copol_ddma = ((1 - delay_frac) * brcs_copol[doppler_intg, delay_intg] +
+                               delay_frac * brcs_copol[doppler_intg, delay_intg + 1])
+
+            brcs_xpol_ddma = ((1 - delay_frac) * brcs_xpol[doppler_intg, delay_intg] +
+                              delay_frac * brcs_xpol[doppler_intg, delay_intg + 1])
+
+            A_eff_ddma = ((1 - delay_frac) * A_eff[doppler_intg, delay_intg] +
+                          delay_frac * A_eff[doppler_intg, delay_intg + 1])
+
+    return brcs_copol_ddma, brcs_xpol_ddma, A_eff_ddma
+
+
+def get_ddma_v2(brcs_copol, brcs_xpol, A_eff, sp_delay_row, sp_doppler_col):
+    """
+    this function gets the brcs and A_eff within ddma region - 3*3 bin
+
+    Parameters
+    ----------
+    brcs_copol
+    brcs_xpol
+    A_eff
+    sp_delay_row
+    sp_doppler_col
+
+    Returns
+    -------
+    brcs_copol_ddma
+    brcs_xpol_ddma
+    A_eff_ddma
+    """
+
+    delay_intg = math.floor(sp_delay_row)
+    delay_frac = sp_delay_row - math.floor(sp_delay_row)
+
+    doppler_intg = round(sp_doppler_col)
+    doppler_frac = sp_doppler_col - round(sp_doppler_col)
+
+    if delay_intg >= 2:
+        delay_range = range(delay_intg - 2, delay_intg + 2)
+    elif delay_intg < 2:
+        delay_range = range(1, 5)
+
+    if doppler_frac >= 0:
+
+        if doppler_intg < 3 and doppler_intg >= 1:
+            doppler_range = range(doppler_intg - 1, doppler_intg + 3)
+
+        elif doppler_intg >= 3:
+            doppler_range = range(2, 6)
+
+        elif doppler_intg < 1:
+            doppler_range = range(1, 5)
+
+        brcs_copol_ddma = (
+                (1 - delay_frac) * (1 - doppler_frac) * brcs_copol[doppler_range[0], delay_range[0]] +
+                (1 - delay_frac) * brcs_copol[doppler_range[1], delay_range[0]] +
+                (1 - delay_frac) * brcs_copol[doppler_range[2], delay_range[0]] +
+                (1 - delay_frac) * doppler_frac * brcs_copol[doppler_range[3], delay_range[0]] +
+                (1 - doppler_frac) * brcs_copol[doppler_range[0], delay_range[1]] +
+                brcs_copol[doppler_range[1], delay_range[1]] +
+                brcs_copol[doppler_range[2], delay_range[1]] +
+                doppler_frac * brcs_copol[doppler_range[3], delay_range[1]] +
+                (1 - doppler_frac) * brcs_copol[doppler_range[0], delay_range[2]] +
+                brcs_copol[doppler_range[1], delay_range[2]] +
+                brcs_copol[doppler_range[2], delay_range[2]] +
+                doppler_frac * brcs_copol[doppler_range[3], delay_range[2]] +
+                (1 - doppler_frac) * delay_frac * brcs_copol[doppler_range[0], delay_range[3]] +
+                delay_frac * brcs_copol[doppler_range[1], delay_range[3]] +
+                delay_frac * brcs_copol[doppler_range[2], delay_range[3]] +
+                delay_frac * doppler_frac * brcs_copol[doppler_range[3], delay_range[3]]
+        )
+
+        brcs_xpol_ddma = (
+                (1 - delay_frac) * (1 - doppler_frac) * brcs_xpol[doppler_range[0], delay_range[0]] +
+                (1 - delay_frac) * brcs_xpol[doppler_range[1], delay_range[0]] +
+                (1 - delay_frac) * brcs_xpol[doppler_range[2], delay_range[0]] +
+                (1 - delay_frac) * doppler_frac * brcs_xpol[doppler_range[3], delay_range[0]] +
+                (1 - doppler_frac) * brcs_xpol[doppler_range[0], delay_range[1]] +
+                brcs_xpol[doppler_range[1], delay_range[1]] +
+                brcs_xpol[doppler_range[2], delay_range[1]] +
+                doppler_frac * brcs_xpol[doppler_range[3], delay_range[1]] +
+                (1 - doppler_frac) * brcs_xpol[doppler_range[0], delay_range[2]] +
+                brcs_xpol[doppler_range[1], delay_range[2]] +
+                brcs_xpol[doppler_range[2], delay_range[2]] +
+                doppler_frac * brcs_xpol[doppler_range[3], delay_range[2]] +
+                (1 - doppler_frac) * delay_frac * brcs_xpol[doppler_range[0], delay_range[3]] +
+                delay_frac * brcs_xpol[doppler_range[1], delay_range[3]] +
+                delay_frac * brcs_xpol[doppler_range[2], delay_range[3]] +
+                delay_frac * doppler_frac * brcs_xpol[doppler_range[3], delay_range[3]]
+        )
+
+        A_eff_ddma = (
+                (1 - delay_frac) * (1 - doppler_frac) * A_eff[doppler_range[0], delay_range[0]] +
+                (1 - delay_frac) * A_eff[doppler_range[1], delay_range[0]] +
+                (1 - delay_frac) * A_eff[doppler_range[2], delay_range[0]] +
+                (1 - delay_frac) * doppler_frac * A_eff[doppler_range[3], delay_range[0]] +
+                (1 - doppler_frac) * A_eff[doppler_range[0], delay_range[1]] +
+                A_eff[doppler_range[1], delay_range[1]] +
+                A_eff[doppler_range[2], delay_range[1]] +
+                doppler_frac * A_eff[doppler_range[3], delay_range[1]] +
+                (1 - doppler_frac) * A_eff[doppler_range[0], delay_range[2]] +
+                A_eff[doppler_range[1], delay_range[2]] +
+                A_eff[doppler_range[2], delay_range[2]] +
+                doppler_frac * A_eff[doppler_range[3], delay_range[2]] +
+                (1 - doppler_frac) * delay_frac * A_eff[doppler_range[0], delay_range[3]] +
+                delay_frac * A_eff[doppler_range[1], delay_range[3]] +
+                delay_frac * A_eff[doppler_range[2], delay_range[3]] +
+                delay_frac * doppler_frac * A_eff[doppler_range[3], delay_range[3]]
+        )
+
+    elif doppler_frac < 0:
+
+        if doppler_intg <= 3 and doppler_intg > 1:
+            doppler_range = range(doppler_intg - 2, doppler_intg + 2)
+
+        elif doppler_intg > 3:
+            doppler_range = range(2, 6)
+
+        elif doppler_intg <= 1:
+            doppler_range = range(1, 5)
+
+        brcs_copol_ddma = (
+                (1 - delay_frac) * abs(doppler_frac) * brcs_copol[doppler_range[0], delay_range[0]] +
+                (1 - delay_frac) * brcs_copol[doppler_range[1], delay_range[0]] +
+                (1 - delay_frac) * brcs_copol[doppler_range[2], delay_range[0]] +
+                (1 - delay_frac) * (1 - abs(doppler_frac)) * brcs_copol[doppler_range[3], delay_range[0]] +
+                abs(doppler_frac) * brcs_copol[doppler_range[0], delay_range[1]] +
+                brcs_copol[doppler_range[1], delay_range[1]] +
+                brcs_copol[doppler_range[2], delay_range[1]] +
+                (1 - abs(doppler_frac)) * brcs_copol[doppler_range[3], delay_range[1]] +
+                abs(doppler_frac) * brcs_copol[doppler_range[0], delay_range[2]] +
+                brcs_copol[doppler_range[1], delay_range[2]] +
+                brcs_copol[doppler_range[2], delay_range[2]] +
+                (1 - abs(doppler_frac)) * brcs_copol[doppler_range[3], delay_range[2]] +
+                abs(doppler_frac) * delay_frac * brcs_copol[doppler_range[0], delay_range[3]] +
+                delay_frac * brcs_copol[doppler_range[1], delay_range[3]] +
+                delay_frac * brcs_copol[doppler_range[2], delay_range[3]] +
+                delay_frac * (1 - abs(doppler_frac)) * brcs_copol[doppler_range[3], delay_range[3]]
+        )
+
+        brcs_xpol_ddma = (
+                (1 - delay_frac) * abs(doppler_frac) * brcs_xpol[doppler_range[0], delay_range[0]] +
+                (1 - delay_frac) * brcs_xpol[doppler_range[1], delay_range[0]] +
+                (1 - delay_frac) * brcs_xpol[doppler_range[2], delay_range[0]] +
+                (1 - delay_frac) * (1 - abs(doppler_frac)) * brcs_xpol[doppler_range[3], delay_range[0]] +
+                abs(doppler_frac) * brcs_copol[doppler_range[0], delay_range[1]] +
+                brcs_xpol[doppler_range[1], delay_range[1]] +
+                brcs_xpol[doppler_range[2], delay_range[1]] +
+                (1 - abs(doppler_frac)) * brcs_xpol[doppler_range[3], delay_range[1]] +
+                abs(doppler_frac) * brcs_xpol[doppler_range[0], delay_range[2]] +
+                brcs_xpol[doppler_range[1], delay_range[2]] +
+                brcs_xpol[doppler_range[2], delay_range[2]] +
+                (1 - abs(doppler_frac)) * brcs_xpol[doppler_range[3], delay_range[2]] +
+                abs(doppler_frac) * delay_frac * brcs_xpol[doppler_range[0], delay_range[3]] +
+                delay_frac * brcs_xpol[doppler_range[1], delay_range[3]] +
+                delay_frac * brcs_xpol[doppler_range[2], delay_range[3]] +
+                delay_frac * (1 - abs(doppler_frac)) * brcs_xpol[doppler_range[3], delay_range[3]]
+        )
+
+        A_eff_ddma = (
+                (1 - delay_frac) * abs(doppler_frac) * A_eff[doppler_range[0], delay_range[0]] +
+                (1 - delay_frac) * A_eff[doppler_range[1], delay_range[0]] +
+                (1 - delay_frac) * A_eff[doppler_range[2], delay_range[0]] +
+                (1 - delay_frac) * (1 - abs(doppler_frac)) * A_eff[doppler_range[3], delay_range[0]] +
+                abs(doppler_frac) * A_eff[doppler_range[0], delay_range[1]] +
+                A_eff[doppler_range[1], delay_range[1]] +
+                A_eff[doppler_range[2], delay_range[1]] +
+                (1 - abs(doppler_frac)) * A_eff[doppler_range[3], delay_range[1]] +
+                abs(doppler_frac) * A_eff[doppler_range[0], delay_range[2]] +
+                A_eff[doppler_range[1], delay_range[2]] +
+                A_eff[doppler_range[2], delay_range[2]] +
+                (1 - abs(doppler_frac)) * A_eff[doppler_range[3], delay_range[2]] +
+                abs(doppler_frac) * delay_frac * A_eff[doppler_range[0], delay_range[3]] +
+                delay_frac * A_eff[doppler_range[1], delay_range[3]] +
+                delay_frac * A_eff[doppler_range[2], delay_range[3]] +
+                delay_frac * (1 - abs(doppler_frac)) * A_eff[doppler_range[3], delay_range[3]]
+        )
+
+    return brcs_copol_ddma, brcs_xpol_ddma, A_eff_ddma
+
+
+
 def get_amb_fun(dtau_s, dfreq_Hz, tau_c, Ti):
     """
     this function computes the ambiguity function
